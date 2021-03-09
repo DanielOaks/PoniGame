922134d1153e591e101e4ba5b4b134b1fcd7b7bd
Daniel Oaks <daniel@danieloaks.net>
2021-03-10 01:06:43 +1000

    Implement basic blink teleporting

---
+++ PonyGame/Assets/Scripts/Characters/TS/TSTeleport.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TSTeleport : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask m_lineOfSightBlocking;
+
+    [SerializeField]
+    [Range(0, 2)]
+    private float m_minDistance = 1f;
+
+    [SerializeField]
+    private float m_maxDistance = 100f;
+
+    private TSMagic m_magic;
+    private GameObject m_twi;
+
+    private void Start()
+    {
+        m_magic = GetComponent<TSMagic>();
+        m_twi = GameObject.FindWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_magic.CurrentSpell == "teleport" && Controls.JustDown(GameButton.TK) && !m_magic.IsUsingMagic)
+        {
+            // try to teleport
+            Debug.Log("Trying to teleport");
+            Vector3? hit = FindTeleportHit();
+            if (hit != null)
+            {
+                m_twi.transform.position = (Vector3)hit;
+            }
+        }
+    }
+
+    private Vector3? FindTeleportHit()
+    {
+        Transform cam = Camera.main.transform;
+        RaycastHit[] hits = Physics.RaycastAll(cam.position, cam.forward, m_maxDistance, m_lineOfSightBlocking);
+        foreach (var hit in hits)
+        {
+            // only return the first hit lmao
+            return hit.point;
+        }
+        return null;
+    }
+}
